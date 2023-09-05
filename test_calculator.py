92a2b738ad9f04d2acd2515230e544476bead07e
chughtaie <hafizchughtai09@gmail.com>
2023-09-05 10:53:50 -0700

    test updated

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,5 @@ from calculator import  multiply
 def test_multiply(): 
     assert multiply(2,6) == 12 
     assert multiply(2.5,-5) == -12.5 
-    assert multiply(3,6) == 18
     assert multiply(3,7) == 21
     assert multiply(3,9) == 27
