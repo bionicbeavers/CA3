b3c7eebd9b807c1b783c67b353a372147d56b798
Ahmed Iqbal <ahmediqbal.dev@gmail.com>
2023-09-05 22:42:57 +0500

    Added subtraction to calculator and test

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import  multiply
+from calculator import  multiply, sub
 
 def test_multiply(): 
     assert multiply(2,6) == 12 
@@ -6,3 +6,10 @@ def test_multiply():
     assert multiply(3,6) == 18
     assert multiply(3,7) == 21
     assert multiply(3,9) == 27
+
+def test_sub():
+    assert sub(2,6) == -4
+    assert sub(2.5,-5) == 7.5
+    assert sub(3,6) == -3
+    assert sub(3,7) == -4
+    assert sub(3,9) == -6
