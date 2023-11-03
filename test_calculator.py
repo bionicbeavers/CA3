4391edccc473f84d0f1ca9e318956eb2c0efe039
HissamSavul <hissamsavul111@gmail.com>
2023-11-03 15:21:28 +0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -2,24 +2,24 @@
 from calculator import add
 
 def test_add():
-    assert add(2, 3) == 5
-    assert add(2, 2) == 4
-    assert add(2, 1) == 3
-    assert add(1, 1) == 2
+    assert add(2, 3) == '5'
+    assert add(2, 2) == '4'
+    assert add(2, 1) == '3'
+    assert add(1, 1) == '2'
 
 from calculator import  multiply, sub
 
 def test_multiply(): 
-    assert multiply(2,6) == 12 
-    assert multiply(2.5,-5) == -12.5 
-    assert multiply(3,8) == 24
-    assert multiply(3,9) == 27
+    assert multiply(2,6) == '12' 
+    assert multiply(2.5,-5) == '-12.5' 
+    assert multiply(3,8) == '24'
+    assert multiply(3,9) == '27'
 
 def test_sub():
-    assert sub(2,6) == -4
-    assert sub(2.5,-5) == 7.5
-    assert sub(3,6) == -3
-    assert sub(3,7) == -4
-    assert sub(3,9) == -6
-    assert sub(5,5) == 0
+    assert sub(2,6) == '-4'
+    assert sub(2.5,-5) == '7.5'
+    assert sub(3,6) == '-3'
+    assert sub(3,7) == '-4'
+    assert sub(3,9) == '-6'
+    assert sub(5,5) == '0'
 
