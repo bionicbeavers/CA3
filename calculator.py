575c1308f0264ffb1d867c1652c4491d0120d7b0
HissamSavul <hissamsavul111@gmail.com>
2024-02-13 12:24:32 +0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return "Welcome to calculator App"
+    return "Welcome to calculator Apps"
 
 @app.route('/calc/sub/<int:x>/<int:y>')
 def sub(x, y):
